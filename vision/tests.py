0b33266264fdc8351db663b8daafababece2e2cc
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-04-18 22:20:42 -0400

    Upload test img, write test for vision main

--- vision/tests.py
+++ vision/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase\
+from vision.vision import main
 
 # Create your tests here.
+
+
+class VisionTestCase(TestCase):
+    def test_main(self):
+        """
+        Test the main function of the vision module.
+        """
+        # Test the main function with a sample jpeg file.
+        jpeg_file = "vision/test_images/my_books_01.jpeg"
+        spines = main(jpeg_file)
+        self.assertEqual(len(spines), 1)
+        self.assertEqual(spines[0].text, "The Hidden Palace Helene Wecker")
+        self.assertEqual(spines[0].height, 3000)
+        self.assertEqual(spines[0].thickness, 582)
+        self.assertEqual(spines[0].background_color, (37, 37, 37))
+        self.assertEqual(spines[0].text_color, (199, 170, 123))
+    
--- vision/vision.py
+++ vision/vision.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+
+def main(jpeg_file):
+    """
+    Main function for the vision module.
+
+    Args:
+        jpeg_file: The path to the jpeg file to be processed.
+
+    Returns:
+        A list of "Spine" objects, each of which represents the following:
+            - The spine's text content.
+            - The spine's dimensions (height and thickness in pixels)
+            - The spine's background color as a set of RGB values.
+            - the spine's text color as a set of RGB values.
+    """
+    
