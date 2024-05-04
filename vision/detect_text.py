2c83fd69d1d656654d45bcdf320f4d7a88ac15ed
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-05-04 18:33:18 -0400

    Refactor detect_text.py
    Calc brightness inside adj_bright func

--- vision/detect_text.py
+++ vision/detect_text.py
@@ -5,8 +5,18 @@ import numpy as np
 import easyocr as ocr
 
 
-
 def detect_text(image_path):
+    """
+    This function detects text in an image and returns the OCR results. Images are preprocessed, then text detection 
+    is applied 4 times per image to account for a 90 degree counter-clockwise rotation and with/without a threshold 
+    applied. 
+    
+    Args:
+        image_path (str): The path to the image file.
+        
+    Returns:
+        list: A list of strings containing the detected text.
+    """
     reader = ocr.Reader(lang_list=['en'], gpu=True)
     results = []
 
@@ -30,7 +40,42 @@ def detect_text(image_path):
     return results
 
 
+def preprocess(image, threshold=True):
+    """
+    This function preprocesses an image by adjusting the brightness, normalizing the image, removing noise, converting to
+    grayscale, and smoothing. A threshold can be applied to the image for potential text detection improvements.
+
+    Args:
+        image (numpy.ndarray): The image array.
+        threshold (bool): A boolean indicating whether to apply a threshold to the image.
+
+    Returns:
+        numpy.ndarray: The preprocessed image array.
+    """
+    img = adjust_brightness(image)
+    norm_img = np.zeros((image.shape[0], image.shape[1]))
+    img = cv.normalize(image, norm_img, 0, 255, cv.NORM_MINMAX)
+    img = cv.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 15)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    if threshold:
+        _, img = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    img = cv.GaussianBlur(img, (5, 5), 0)
+    
+    return img
+
+
 def draw_bounding_boxes(image, detections):
+    """
+    This function draws bounding boxes around the detected text in an image. It saves the image with the bounding boxes 
+    in the 'vision/images/detection_temp' directory.
+
+    Args:
+        image (numpy.ndarray): The image array.
+        detections (list): A list of tuples containing the bounding box coordinates, text, and confidence score.
+
+    Returns:
+        None
+    """
     image_with_boxes = image.copy()
     
     for detection in detections:
@@ -45,85 +90,26 @@ def draw_bounding_boxes(image, detections):
     cv.imwrite(f"vision/images/detection_temp/debug_images/text_detection_{timestamp}.jpeg", image_with_boxes)
 
 
-def calculate_brightness(image):
-    # Check if the image is already in grayscale
-    if len(image.shape) == 2 or image.shape[2] == 1:  # Grayscale image
+def adjust_brightness(image):
+    """
+    This function adjusts the brightness of an image by calculating the average brightness and applying a factor to
+    normalize the image.
+
+    Args:
+        image (numpy.ndarray): The image array.
+
+    Returns:
+        numpy.ndarray: The adjusted image array.
+    """
+    if len(image.shape) == 2 or image.shape[2] == 1:
         grayscale_image = image
     else:
         grayscale_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    return cv.mean(grayscale_image)[0]
+    brightness = cv.mean(grayscale_image)[0]
 
-
-def adjust_brightness(image, brightness):
     img_float = image.astype(np.float32)
     factor = 127 / brightness 
     brightened_image = cv.multiply(img_float, factor)
     brightened_image = np.clip(brightened_image, 0, 255) 
+
     return brightened_image.astype(np.uint8)
-
-
-def preprocess(image, threshold=True):
-    # Adjust the brightness of the image
-    brightness = calculate_brightness(image)
-    image = adjust_brightness(image, brightness)
-
-    # Normalize the image
-    norm_img = np.zeros((image.shape[0], image.shape[1]))
-    img = cv.normalize(image, norm_img, 0, 255, cv.NORM_MINMAX)
-
-    # Noise removal
-    img = cv.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 15)
-
-    # Convert to grayscale
-    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-    # Thresholding
-    if threshold:
-        _, img = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-
-    # Smoothing
-    img = cv.GaussianBlur(img, (5, 5), 0)
-    
-    return img
-
-
-def detect_text(image_path):
-    reader = ocr.Reader(lang_list=['en'], gpu=True)
-    results = []
-    
-    original_image = cv.imread(image_path)
-    preprocessed_image = preprocess(original_image, True)
-    result_00 = reader.readtext(preprocessed_image, detail=1)
-    draw_bounding_boxes(preprocessed_image, result_00)
-
-    # Rotate original image once and then preprocess
-    rotated_image = cv.rotate(original_image, cv.ROTATE_90_COUNTERCLOCKWISE)
-    preprocessed_image_90 = preprocess(rotated_image, True)
-    result_90 = reader.readtext(preprocessed_image_90, detail=1)
-    draw_bounding_boxes(preprocessed_image_90, result_90)
-
-    # If processing without threshold, do it from the original image
-    image_00_no_thresh = preprocess(original_image, False)
-    result_00_no_thresh = reader.readtext(image_00_no_thresh, detail=1)
-    draw_bounding_boxes(image_00_no_thresh, result_00_no_thresh)
-
-    image_90_no_thresh = preprocess(rotated_image, False)
-    result_90_no_thresh = reader.readtext(image_90_no_thresh, detail=1)
-    draw_bounding_boxes(image_90_no_thresh, result_90_no_thresh)
-
-    book_text_list = [text for bbox, text, _ in result_00]
-    book_text_list += [text for bbox, text, _ in result_90]
-    book_text_list += [text for bbox, text, _ in result_00_no_thresh]
-    book_text_list += [text for bbox, text, _ in result_90_no_thresh]
-
-    book_text_list = [text for text in book_text_list if len(text) > 2]
-    book_text_list = list(set(book_text_list)) # Remove dupes
-
-    # Remove all non-alphanumeric characters
-    book_text_list = ["".join(c for c in text if c.isalnum() or c.isspace()) for text in book_text_list]
-
-    book_text_string = ", ".join(book_text_list)
-
-    results.append(book_text_string)
-
-    return results
