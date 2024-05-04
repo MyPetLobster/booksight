cf4e0940c2a456d1c4987dbeb3f9046b3d845c92
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-05-04 17:59:13 -0400

    Refactor detect_text to remove repetition

--- vision/detect_text.py
+++ vision/detect_text.py
@@ -5,41 +5,24 @@ import numpy as np
 import easyocr as ocr
 
 
+
 def detect_text(image_path):
     reader = ocr.Reader(lang_list=['en'], gpu=True)
     results = []
-    
+
+    def process_image(image, with_threshold):
+        preprocessed_image = preprocess(image, with_threshold)
+        result = reader.readtext(preprocessed_image, detail=1)
+        draw_bounding_boxes(preprocessed_image, result)
+        return [text for bbox, text, _ in result if len(text) > 2]
+
     original_image = cv.imread(image_path)
-    preprocessed_image = preprocess(original_image, True)
-    result_00 = reader.readtext(preprocessed_image, detail=1)
-    draw_bounding_boxes(preprocessed_image, result_00)
+    book_text_list = process_image(original_image, True)
+    book_text_list += process_image(cv.rotate(original_image, cv.ROTATE_90_COUNTERCLOCKWISE), True)
+    book_text_list += process_image(original_image, False)
+    book_text_list += process_image(cv.rotate(original_image, cv.ROTATE_90_COUNTERCLOCKWISE), False)
 
-    # Rotate original image once and then preprocess
-    rotated_image = cv.rotate(original_image, cv.ROTATE_90_COUNTERCLOCKWISE)
-    preprocessed_image_90 = preprocess(rotated_image, True)
-    result_90 = reader.readtext(preprocessed_image_90, detail=1)
-    draw_bounding_boxes(preprocessed_image_90, result_90)
-
-    # If processing without threshold, do it from the original image
-    image_00_no_thresh = preprocess(original_image, False)
-    result_00_no_thresh = reader.readtext(image_00_no_thresh, detail=1)
-    draw_bounding_boxes(image_00_no_thresh, result_00_no_thresh)
-
-    image_90_no_thresh = preprocess(rotated_image, False)
-    result_90_no_thresh = reader.readtext(image_90_no_thresh, detail=1)
-    draw_bounding_boxes(image_90_no_thresh, result_90_no_thresh)
-
-    book_text_list = [text for bbox, text, _ in result_00]
-    book_text_list += [text for bbox, text, _ in result_90]
-    book_text_list += [text for bbox, text, _ in result_00_no_thresh]
-    book_text_list += [text for bbox, text, _ in result_90_no_thresh]
-
-    book_text_list = [text for text in book_text_list if len(text) > 2]
-    book_text_list = list(set(book_text_list)) # Remove dupes
-
-    # Remove all non-alphanumeric characters
-    book_text_list = ["".join(c for c in text if c.isalnum() or c.isspace()) for text in book_text_list]
-
+    book_text_list = list(set([text for text in book_text_list if text.isalnum() or text.isspace()]))
     book_text_string = ", ".join(book_text_list)
 
     results.append(book_text_string)
