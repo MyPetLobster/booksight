921c982d4a94936ea08c0258e60b3aa65118d47e
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-05-08 17:08:44 -0400

    Add new test img, rename test 5 jpeg

--- vision/vision.py
+++ vision/vision.py
@@ -226,4 +226,4 @@ if __name__ == "__main__":
     # for book in books:
     #     util.log_print(book)
     #     util.log_print("\n")
-    vision("vision/images/test_images/test_five_books.jpeg")
+    vision("vision/images/test_images/mom_test.jpeg")
