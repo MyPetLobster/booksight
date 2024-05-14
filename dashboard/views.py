e8d75a231fcafec6166378efbe00652b3e406c5c
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-05-14 16:17:35 -0400

    Add vision view, url, html page

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,20 +4,32 @@ from .models import Scan
 
 
 def index(request):
-    if request.method == 'POST':
-        image = request.FILES.get('uploaded-image')
-        print(f"image: {image}")
-
-        new_scan = Scan.objects.create()
-        new_scan.uploaded_image = image
-        new_scan.save()
-
-        print(new_scan.uploaded_image.url)
-
     return render(request, 'index.html', {
         'scans': [scan.serialize() for scan in Scan.objects.all()]
     })
 
+
+def vision(request):
+    if request.method == 'POST':
+        image = request.FILES.get('uploaded-image')
+
+        if not image:
+            return render(request, 'index.html', {
+                'error': 'No image uploaded'
+            })
+        
+        new_scan = Scan.objects.create()
+        new_scan.uploaded_image = image
+        new_scan.save()
+
+        return render(request, 'vision.html', {
+            'scan': new_scan.serialize()
+        })
+
+    else: # GET request
+        return render(request, 'index.html', {
+            'error': 'Invalid request method'
+        })
 
 
 def about(request):
