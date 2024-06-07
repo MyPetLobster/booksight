ee3213892053ac0ae6d5a916e7043058a172fa3c
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-06-06 21:41:35 -0400

    Put page scripts within if-pageID checks
    Edit text in vis_complete

--- dashboard/static/dashboard/javascript/scripts.js
+++ dashboard/static/dashboard/javascript/scripts.js
@@ -56,12 +56,9 @@ window.onscroll = function(ev) {
 
 
 /// TIPS/ABOUT - Small logo interactivity
-
-const pageIdentifierText = document.querySelector(".page-identifier").textContent;
-
 const animatedPages = ["tips", "about", "vision-complete", "vision-failed"];
 
-if (animatedPages.includes(pageIdentifierText)) {
+if (animatedPages.includes(pageIdentifier)) {
     const logoEyes = document.querySelector("#logo-eyes-vision");
     const logoText = document.querySelector("#logo-text-vision");
     const smallLogoElements = [logoEyes, logoText];
@@ -94,141 +91,143 @@ if (animatedPages.includes(pageIdentifierText)) {
 
 /// INDEX PAGE SCRIPTS
 
-// Checkboxes for export formats - index form
-const selectAll = document.getElementById('all');
-const csv = document.getElementById('csv');
-const json = document.getElementById('json');
-const xml = document.getElementById('xml');
-const txt = document.getElementById('txt');
+if (pageIdentifier === 'index') {
+    // Checkboxes for export formats - index form
+    const selectAll = document.getElementById('all');
+    const csv = document.getElementById('csv');
+    const json = document.getElementById('json');
+    const xml = document.getElementById('xml');
+    const txt = document.getElementById('txt');
 
-selectAll.addEventListener('change', () => {
-    if (selectAll.checked) {
-        csv.checked = true;
-        json.checked = true;
-        xml.checked = true;
-        txt.checked = true;
-    } else {
-        csv.checked = false;
-        json.checked = false;
-        xml.checked = false;
-        txt.checked = false;
-    }
-})
-
-options = [csv, json, xml, txt];
-for (let option of options) {
-    option.addEventListener('change', () => {
-        if (csv.checked && json.checked && xml.checked && txt.checked) {
-            selectAll.checked = true;
+    selectAll.addEventListener('change', () => {
+        if (selectAll.checked) {
+            csv.checked = true;
+            json.checked = true;
+            xml.checked = true;
+            txt.checked = true;
         } else {
-            selectAll.checked = false;
+            csv.checked = false;
+            json.checked = false;
+            xml.checked = false;
+            txt.checked = false;
         }
     })
-}
 
+    options = [csv, json, xml, txt];
 
-// Index Form and Landing Logo Interplay, Form Handling
-const logoEyes = document.getElementById('logo-eyes');
-const logoText = document.getElementById('logo-text');
-const hiddenSubtitle = document.querySelector('.hidden-subtitle');
-const floatingForm = document.querySelector('.upload-form-div');
-const landingLogoDiv = document.querySelector('.landing-logo-div');
-const uploadImageForm = document.getElementById('upload-img-form');
-const cancelBtn = document.querySelector('.btn-box button');
-const advOptionsButton = document.getElementById('adv-options-button');
-const advOptionsDiv = document.getElementById('adv-options-div');
-const advOptionsClose = document.getElementById('adv-options-close');
-const screenWidth = window.innerWidth;
-
-if (screenWidth > 600) {
-    // Desktop behavior
-    [logoEyes, logoText].forEach((element) => {
-        element.addEventListener('mouseover', () => {
-            logoText.style.maxWidth = "110%";
-            logoText.style.opacity = 1;
-            logoEyes.style.maxWidth = "70%";
-            logoEyes.style.opacity = 1;
-            hiddenSubtitle.style.opacity = 1;
-            hiddenSubtitle.style.transition = "10s";
+    for (let option of options) {
+        option.addEventListener('change', () => {
+            if (csv.checked && json.checked && xml.checked && txt.checked) {
+                selectAll.checked = true;
+            } else {
+                selectAll.checked = false;
+            }
         })
-        element.addEventListener('mouseout', () => {
-            logoText.style.maxWidth = "100%";
-            logoText.style.opacity = 0.15;
-            logoEyes.style.maxWidth = "60%";
-            logoEyes.style.opacity = 0.15;
-            hiddenSubtitle.style.opacity = 0;
-            hiddenSubtitle.style.transition = "2s";
+    }
+
+
+    // Index Form and Landing Logo Interplay, Form Handling
+    const logoEyes = document.getElementById('logo-eyes');
+    const logoText = document.getElementById('logo-text');
+    const hiddenSubtitle = document.querySelector('.hidden-subtitle');
+    const floatingForm = document.querySelector('.upload-form-div');
+    const landingLogoDiv = document.querySelector('.landing-logo-div');
+    const cancelBtn = document.querySelector('.btn-box button');
+    const advOptionsButton = document.getElementById('adv-options-button');
+    const advOptionsDiv = document.getElementById('adv-options-div');
+    const advOptionsClose = document.getElementById('adv-options-close');
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth > 600) {
+        // Desktop behavior
+        [logoEyes, logoText].forEach((element) => {
+            element.addEventListener('mouseover', () => {
+                logoText.style.maxWidth = "110%";
+                logoText.style.opacity = 1;
+                logoEyes.style.maxWidth = "70%";
+                logoEyes.style.opacity = 1;
+                hiddenSubtitle.style.opacity = 1;
+                hiddenSubtitle.style.transition = "10s";
+            })
+            element.addEventListener('mouseout', () => {
+                logoText.style.maxWidth = "100%";
+                logoText.style.opacity = 0.15;
+                logoEyes.style.maxWidth = "60%";
+                logoEyes.style.opacity = 0.15;
+                hiddenSubtitle.style.opacity = 0;
+                hiddenSubtitle.style.transition = "2s";
+            })
         })
-    })
-    landingLogoDiv.addEventListener('click', () => {
-        floatingForm.style.opacity = 1;
-        floatingForm.style.zIndex = 999;
-        landingLogoDiv.style.opacity = 0;
-    })
-} else {
-    // Mobile behavior
-    landingLogoDiv.addEventListener('click', () => {
-        logoEyes.style.transition = "2s";
-        logoEyes.style.opacity = 1;
-        logoText.style.transition = "2s";
-        logoText.style.opacity = 1;
-        hiddenSubtitle.style.transition = "2s";
-        hiddenSubtitle.style.opacity = 1;
-
-        const footer = document.getElementById('footer');
-        footer.style.opacity = 0;
-
-        const owlLogoNavItem = document.getElementById('owl-logo-nav-item');
-        if (owlLogoNavItem) {
-            owlLogoNavItem.style.transition = "2s";
-            owlLogoNavItem.style.opacity = 0;
-            owlLogoNavItem.style.zIndex = -1;
-        }
-
-        setTimeout(() => {
-            logoEyes.style.transition = "2s";
-            logoEyes.style.opacity = 0.15;
-            logoText.style.transition = "2s";
-            logoText.style.opacity = 0.15;
-            hiddenSubtitle.style.transition = "2s";
-            hiddenSubtitle.style.opacity = 0;
+        landingLogoDiv.addEventListener('click', () => {
             floatingForm.style.opacity = 1;
             floatingForm.style.zIndex = 999;
             landingLogoDiv.style.opacity = 0;
-        }, 2000)
-    })
-}
+        })
+    } else {
+        // Mobile behavior
+        landingLogoDiv.addEventListener('click', () => {
+            logoEyes.style.transition = "2s";
+            logoEyes.style.opacity = 1;
+            logoText.style.transition = "2s";
+            logoText.style.opacity = 1;
+            hiddenSubtitle.style.transition = "2s";
+            hiddenSubtitle.style.opacity = 1;
 
-cancelBtn.addEventListener('click', () => {
-    floatingForm.style.opacity = 0;
-    floatingForm.style.zIndex = -1;
-    landingLogoDiv.style.transition = "2s";
-    landingLogoDiv.style.opacity = 1;
-    logoEyes.style.transition = "2s";
-    logoEyes.style.opacity = 0.15;
-    logoText.style.transition = "2s";
-    logoText.style.opacity = 0.15;
+            const footer = document.getElementById('footer');
+            footer.style.opacity = 0;
 
-    window.scrollTo(0, 0);
+            const owlLogoNavItem = document.getElementById('owl-logo-nav-item');
+            if (owlLogoNavItem) {
+                owlLogoNavItem.style.transition = "2s";
+                owlLogoNavItem.style.opacity = 0;
+                owlLogoNavItem.style.zIndex = -1;
+            }
 
-    const footer = document.getElementById('footer');
-    footer.style.opacity = 1;
-    const owlLogoNavItem = document.getElementById('owl-logo-nav-item');
-    if (owlLogoNavItem) {
-        owlLogoNavItem.style.transition = "2s";
-        owlLogoNavItem.style.opacity = 1;
-        owlLogoNavItem.style.zIndex = 1;
+            setTimeout(() => {
+                logoEyes.style.transition = "2s";
+                logoEyes.style.opacity = 0.15;
+                logoText.style.transition = "2s";
+                logoText.style.opacity = 0.15;
+                hiddenSubtitle.style.transition = "2s";
+                hiddenSubtitle.style.opacity = 0;
+                floatingForm.style.opacity = 1;
+                floatingForm.style.zIndex = 999;
+                landingLogoDiv.style.opacity = 0;
+            }, 2000)
+        })
     }
-})
-advOptionsButton.addEventListener('click', () => {
-    advOptionsDiv.style.display = 'flex';
-    advOptionsClose.style.display = 'block';
-    advOptionsButton.style.display = 'none';
-    window.scrollTo(0, document.body.scrollHeight);
-})
-advOptionsClose.addEventListener('click', () => {
-    advOptionsDiv.style.display = 'none';
-    advOptionsClose.style.display = 'none';
-    advOptionsButton.style.display = 'block';
-    window.scrollTo(0, 0);
-})     
+
+    cancelBtn.addEventListener('click', () => {
+        floatingForm.style.opacity = 0;
+        floatingForm.style.zIndex = -1;
+        landingLogoDiv.style.transition = "2s";
+        landingLogoDiv.style.opacity = 1;
+        logoEyes.style.transition = "2s";
+        logoEyes.style.opacity = 0.15;
+        logoText.style.transition = "2s";
+        logoText.style.opacity = 0.15;
+
+        window.scrollTo(0, 0);
+
+        const footer = document.getElementById('footer');
+        footer.style.opacity = 1;
+        const owlLogoNavItem = document.getElementById('owl-logo-nav-item');
+        if (owlLogoNavItem) {
+            owlLogoNavItem.style.transition = "2s";
+            owlLogoNavItem.style.opacity = 1;
+            owlLogoNavItem.style.zIndex = 1;
+        }
+    })
+    advOptionsButton.addEventListener('click', () => {
+        advOptionsDiv.style.display = 'flex';
+        advOptionsClose.style.display = 'block';
+        advOptionsButton.style.display = 'none';
+        window.scrollTo(0, document.body.scrollHeight);
+    })
+    advOptionsClose.addEventListener('click', () => {
+        advOptionsDiv.style.display = 'none';
+        advOptionsClose.style.display = 'none';
+        advOptionsButton.style.display = 'block';
+        window.scrollTo(0, 0);
+    })     
+}
